e70326f9c0d29b741f2079a59ca80fbf6712ede0
Tatiana Andrade <htatiana089@gmail.com>
2026-05-26 03:27:29 -0500

    Initial commit: baseline for Java upgrade

---
+++ src/main/java/com/beauty/always_beautiful/AlwaysBeautifulApplication.java
@@ -0,0 +1,13 @@
+package com.beauty.always_beautiful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlwaysBeautifulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlwaysBeautifulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beauty/always_beautiful/AlwaysBeautifulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauty.always_beautiful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlwaysBeautifulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
